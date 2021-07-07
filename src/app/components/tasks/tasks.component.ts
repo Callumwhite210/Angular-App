d7f2f98dcf9d3132799dfa2c7a00599d9cbb9658
Callum White <callumwhite210@gmail.com>
2021-07-07 17:27:22 +0930

    Mock server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock-tasks';
+import {TaskService} from '../../services/task.service';
 import {Task} from '../../Task';
 
 @Component({
@@ -9,11 +9,15 @@ import {Task} from '../../Task';
 })
 export class TasksComponent implements OnInit {
   //Property of our comonent
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  //add service as a construtor argument
+  constructor(private taskService: TaskService) {}
 
+  //putting it in here because it fires right away
+  //Subscribed to Observable service
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
   }
 
 }
